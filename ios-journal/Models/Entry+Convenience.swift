8630e3382f3973a9aab8abf47129c2a35915bf9a
Dongwoo Pae <dongwoopae@gmail.com>
2019-07-16 22:25:18 -0700

    added failable initializer for firebase data and entryRepresentation computed property

--- ios-journal/Models/Entry+Convenience.swift
+++ ios-journal/Models/Entry+Convenience.swift
@@ -22,6 +22,14 @@ enum Moods: String {
 
 //it does not matter if SQL properties have optional or not it all depends on how initializer is set up. they are set up as none of them are optional so even though SQL's properties have optionals you follow how it is set up here
 extension Entry {
+    
+    var entryRepresentation: EntryRepresentation? {
+        guard let title = title else {return nil}
+        
+        return EntryRepresentation(title: title , bodyText: bodyText ?? "", identifier: identifier ?? "", timestamp: timestamp ?? Date(), mood: mood ?? "")
+    }
+    
+    
     convenience init(title: String, bodyText: String, identifier: String = UUID().uuidString, timestamp: Date = Date(), mood: Moods = .neutral, context: NSManagedObjectContext = CoreDataStack.shared.mainContext) {
         self.init(context: context)
         self.title = title
@@ -30,4 +38,18 @@ extension Entry {
         self.timestamp = timestamp
         self.mood = mood.rawValue
     }
+    
+    //failable initializer
+    convenience init?(entryRepresentation: EntryRepresentation, context: NSManagedObjectContext = CoreDataStack.shared.mainContext) {
+        
+        guard let mood = Moods(rawValue: entryRepresentation.mood) else {return nil}
+        
+        self.init(title: entryRepresentation.title,
+                  bodyText: entryRepresentation.bodyText,
+                  identifier: entryRepresentation.identifier,
+                  timestamp: entryRepresentation.timestamp,
+                  mood: mood,
+                  context: context)
+    }
+    
 }
