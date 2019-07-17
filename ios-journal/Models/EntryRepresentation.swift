edee6d2124272ca2f0547765f5a260c932820178
Dongwoo Pae <dongwoopae@gmail.com>
2019-07-16 21:35:41 -0700

    added == method and EntryRepresentation object

--- ios-journal/Models/EntryRepresentation.swift
+++ ios-journal/Models/EntryRepresentation.swift
@@ -9,6 +9,29 @@
 import Foundation
 
 struct EntryRepresentation: Codable, Equatable {
-    <#fields#>
+    var title: String
+    var bodyText: String
+    var identifier: String  //string not UUID this time
+    var timestamp: Date  //date
+    var mood: String  //enum
 }
 
+func == (lhs: EntryRepresentation, rhs: Entry) -> Bool {
+    return (lhs.title == rhs.title &&
+                lhs.bodyText == rhs.bodyText &&
+                lhs.mood == rhs.mood &&
+                lhs.identifier == rhs.identifier &&
+                lhs.timestamp == rhs.timestamp)
+}
+
+func == (lhs: Entry, rhs: EntryRepresentation) -> Bool {
+    return rhs == lhs
+}
+
+func != (lhs: EntryRepresentation, rhs: Entry) -> Bool {
+    return !(rhs == lhs)
+}
+
+func != (lhs: Entry, rhs: EntryRepresentation) -> Bool {
+    return rhs != lhs
+}
